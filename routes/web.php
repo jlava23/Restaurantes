174a25a443a27f3153cb45733328555e5f778fac
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-10-20 12:35:07 -0500

    Definición de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,38 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MesaController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ReservaController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+
+Route::middleware(['auth'])->group(function () {
+    // Rutas de Perfil
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rutas de Mesas
+    Route::resource('mesas', MesaController::class);
+    
+    // Rutas de Clientes
+    Route::resource('clientes', ClienteController::class);
+
+    // Rutas de Reservas
+    Route::resource('reservas', ReservaController::class);
 });
 
+// Requiere autenticación
 require __DIR__.'/auth.php';
+
