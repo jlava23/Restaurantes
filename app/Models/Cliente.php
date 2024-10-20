1614866aa10118e9f51756adb49a57449732cd62
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-10-19 22:54:34 -0500

    Modelo y relacion de Cliente

---
+++ app/Models/Cliente.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    public function reservas()
+    {
+        return $this->hasMany(Reserva::class);
+    }
+}
