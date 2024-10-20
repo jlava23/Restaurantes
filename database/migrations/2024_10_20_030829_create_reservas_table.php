05b57a4891a792ff6bd74b841f62a176aa710e37
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-10-19 22:11:29 -0500

    Migracion de reservas

---
+++ database/migrations/2024_10_20_030829_create_reservas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mesa_id')->constrained('mesas')->onDelete('cascade');
+            $table->foreignId('cliente_id')->constrained('clientes')->onDelete('cascade');
+            $table->date('fecha_reserva');
+            $table->time('hora_reserva');
+            $table->integer('numero_personas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservas');
+    }
+};
